5ff7e8b37431b1956cfaa9ffee5c37fa71d93629
Андрей Боев <a.boev.it@gmail.com>
2025-10-25 20:27:35 +0300

    Операции деления

---
+++ Операции_деления/Операции_деления.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    // Обычное деление
+    // Целочисленное деление
+    // Остаток от деления (%)
+
+    int a = 9;
+    int b = 5;
+    std::cout << (double)a / b << '\n';
+    std::cout << a * 1.0 / b;
+
+    return 0;
+}
