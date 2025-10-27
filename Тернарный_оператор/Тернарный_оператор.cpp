ab4218bccf5b8e06001e360f6dfeb2ce9e793111
Андрей Боев <a.boev.it@gmail.com>
2025-10-27 20:04:38 +0300

    Тернарный оператор, switch

---
+++ Тернарный_оператор/Тернарный_оператор.cpp
@@ -0,0 +1,12 @@
+
+int main(int argc, char* argv[])
+{
+    /*
+            Тернарный оператор
+
+            условие ? выражение_если_true : выражение_если_true;
+
+            int max = a > b ? a : b;
+     */
+    return 0;
+}
